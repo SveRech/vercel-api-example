f54e675a174b2a1e1624d249e2e426133b2a8f0f
SveRech <127630727+SveRech@users.noreply.github.com>
2025-04-16 19:22:29 +0300

    ывап

--- api/test.js
+++ api/test.js
@@ -3,13 +3,18 @@ const { Pool } = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }, // нужно для Neon
+
 })
 
+
 module.exports = async (req, res) => {
+  console.log('Запрос на /api/db-test принят');
   try {
-    const result = await pool.query('select firstname from public.test')
-    res.status(200).json(result.rows[0])
+    const result = await pool.query('SELECT "FirstName" FROM "public"."TEST" LIMIT 1');
+    console.log('Запрос выполнен успешно', result.rows);
+    res.status(200).json(result.rows[0]);
   } catch (err) {
-    res.status(500).json({ error: err.message })
+    console.error('Ошибка выполнения запроса:', err);
+    res.status(500).json({ error: err.message });
   }
-}
+};
