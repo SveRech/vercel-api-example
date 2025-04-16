597071bb2b9c9a2d036246711afb69778f561e41
SveRech <127630727+SveRech@users.noreply.github.com>
2025-04-16 19:52:40 +0300

    вапывп

--- api/test.js
+++ api/test.js
@@ -1,10 +1,5 @@
+// api/test.js
+
 module.exports = async (req, res) => {
-  console.log('Запрос на /api/test получен');
-  try {
-    // Логика работы с базой данных
-    res.status(200).json({ message: "Запрос прошел успешно" });
-  } catch (err) {
-    console.error('Ошибка в db-test:', err);
-    res.status(500).json({ error: "Ошибка на сервере" });
-  }
+  res.status(200).json({ message: 'Test route is working!' });
 };
