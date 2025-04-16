38ccd6bbabf1a54dc20ee38667d633ed71aef518
SveRech <127630727+SveRech@users.noreply.github.com>
2025-04-16 19:24:19 +0300

    sffsdgd

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,20 @@
-module.exports = (req, res) => {
-  res.status(200).json({ message: "Привет с Vercel!" });
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // нужно для Neon
+
+})
+
+
+module.exports = async (req, res) => {
+  console.log('Запрос на /api/db-test принят');
+  try {
+    const result = await pool.query('SELECT "FirstName" FROM "public"."TEST" LIMIT 1');
+    console.log('Запрос выполнен успешно', result.rows);
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error('Ошибка выполнения запроса:', err);
+    res.status(500).json({ error: err.message });
+  }
 };
